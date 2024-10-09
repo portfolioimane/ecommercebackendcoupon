176b96bb9b9de6d3546201212258847be6657422
Your Name <emawebsolutions.g@gmail.com>
2024-10-09 22:30:13 +0100

    first commit

---
+++ database/migrations/2024_09_21_112452_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->decimal('total_price', 10, 2);
+            $table->decimal('discount', 10, 2)->default(0); // Added discount field with default value of 0
+            $table->string('payment_method'); // Added payment method field
+            $table->string('status')->default('pending'); // Added default status
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
